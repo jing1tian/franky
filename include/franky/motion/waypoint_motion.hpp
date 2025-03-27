9972d65d54ccb6ee6a0e63e0e71c74ea22ab584d
Tim Schneider <tim@robot-learning.de>
2025-03-27 14:15:37 +0100

    Fixed bug in WaypointMotion

--- include/franky/motion/waypoint_motion.hpp
+++ include/franky/motion/waypoint_motion.hpp
@@ -115,9 +115,9 @@ class WaypointMotion : public Motion<ControlSignalType> {
       }
       if (waypoint_iterator_ != waypoints_.end()) {
         prev_result_ = trajectory_generator_.update(input_parameter_, output_parameter_);
-        if (prev_result_ == ruckig::Result::Working) {
+        if (prev_result_ == ruckig::Result::Working || prev_result_ == ruckig::Result::Finished) {
           output_parameter_.pass_to_input(input_parameter_);
-        } else if (prev_result_ != ruckig::Result::Finished) {
+        } else {
           throw MotionPlannerException("Motion planner failed with error code " + std::to_string(prev_result_));
         }
       }
