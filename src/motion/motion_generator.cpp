497c194350a7127b953ddbe72144ca994102206a
Tim Schneider <mail@tim-schneider.me>
2025-04-01 09:08:29 +0200

    Optimized reaction handling and increased reaction recursion limit to 16

--- src/motion/motion_generator.cpp
+++ src/motion/motion_generator.cpp
@@ -50,15 +50,22 @@ MotionGenerator<ControlSignalType>::operator()(const franka::RobotState &robot_s
     auto new_motion = current_motion_->checkAndCallReactions(robot_state, abs_time_ - rel_time_offset_, abs_time_);
     if (new_motion != nullptr) {
       current_motion_ = new_motion;
-      current_motion_->init(robot_, robot_state, previous_command_);
       rel_time_offset_ = abs_time_;
       reaction_fired = true;
       recursion_depth++;
       if (recursion_depth > REACTION_RECURSION_LIMIT) {
+        // This is probably useless, but we make sure that current_motion_ is always initialized, even if an exception
+        // is thrown that should end the current control.
+        current_motion_->init(robot_, robot_state, previous_command_);
         throw ReactionRecursionException(
             "Reaction recursion reached depth limit " + std::to_string(REACTION_RECURSION_LIMIT) + ".");
       }
     }
+  }
+  if (recursion_depth > 0) {
+    // This condition is only met if a reaction has fired. We delay the motion initialization until now to safe time if
+    // multiple reactions fire subsequently.
+    current_motion_->init(robot_, robot_state, previous_command_);
   }
   previous_command_ = current_motion_->nextCommand(robot_state, period, rel_time, abs_time_, previous_command_);
   return previous_command_.value();
